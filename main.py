da6da201ab0924ad8eb17f750042a2e3cdbbdb03
Jooyoung Jeong <jooyoungworknd@gmail.com>
2023-10-19 14:51:48 +0900

    added okt tokenizer for korean

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 from time import time
 import pandas as pd
 
+from konlpy.tag import Okt
+
 def init_model(lang, dim=100, vs=16000):
     
+    if lang == "ko":
+        return Okt()
     s_t = time()
     model = BPEmb(lang=lang, dim=dim, vs=vs)
     e_t = time()
@@ -13,19 +17,24 @@ def init_model(lang, dim=100, vs=16000):
 
 def bleu_score(model, ref_text, cand_text, use_decimal=True, decimal=3):
     
-    # smoothing function for too short n-grams
-    # apply for unigram
-    smooth_method = SmoothingFunction().method1
-    
-    ref_encoded = model.encode(ref_text)
-    cand_encoded = model.encode(cand_text)
-    
-    # weights -> auto_reweight target [unigram, bigram, trigram, fourgram]
-    # auto reweight -> reweigh when the weight condition is satisfied
-    # smoothing function -> smooth function algorithm
-    score = sentence_bleu([ref_encoded], cand_encoded,\
-        weights=[1, 0, 0, 0], auto_reweigh= True, \
-        smoothing_function=smooth_method)
+    if isinstance(model, Okt):
+        score= sentence_bleu([model.morphs(ref_text)], model.morphs(cand_text), \
+            weights=[1, 0, 0, 0], auto_reweigh= True, \
+            smoothing_function=smooth_method)
+    else:
+        # smoothing function for too short n-grams
+        # apply for unigram
+        smooth_method = SmoothingFunction().method1
+        
+        ref_encoded = model.encode(ref_text)
+        cand_encoded = model.encode(cand_text)
+        
+        # weights -> auto_reweight target [unigram, bigram, trigram, fourgram]
+        # auto reweight -> reweigh when the weight condition is satisfied
+        # smoothing function -> smooth function algorithm
+        score = sentence_bleu([ref_encoded], cand_encoded,\
+            weights=[1, 0, 0, 0], auto_reweigh= True, \
+            smoothing_function=smooth_method)
     
     if use_decimal:
         return round(score, decimal)
